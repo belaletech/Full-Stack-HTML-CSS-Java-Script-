afd78ae59a89dc42a491cfd62734a7cd24ad9b45
belaletech <belalahmadswnbhr@gmail.com>
2023-04-16 21:21:24 +0530

    update

---
+++ AgeCalculator/script.js
@@ -0,0 +1,29 @@
+const btnEl = document.getElementById("btn");
+const birthdayEl = document.getElementById("birthday");
+
+const resultEl=document.getElementById("result")
+
+function calculateAge() {
+  const birthdayValue = birthdayEl.value;
+  
+  if (birthdayValue === "") {
+    alert("Please enter your birthday");
+  } else {
+    const age = getAge(birthdayValue);
+    resultEl.innerText=`Your Age is ${age} ${age>1? "years" :"year"} old`;
+  }
+}
+function getAge(birthdayValue) {
+  const currentDate = new Date();
+  const birthdayDate=new Date(birthdayValue);
+  const age=currentDate.getFullYear()-birthdayDate.getFullYear();
+  const month=currentDate.getMonth()-birthdayDate.getMonth();
+
+  if(month<0 || (month===0 && currentDate.getDate()<birthdayDate.getDate()))
+  {
+    age--
+  }
+  return age;
+}
+
+btnEl.addEventListener("click", calculateAge);
