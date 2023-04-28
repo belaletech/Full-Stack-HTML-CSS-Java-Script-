08506719d8117c7d157da1abbabc64ac88b020b5
belaletech <belalahmadswnbhr@gmail.com>
2023-04-29 01:27:05 +0530

    update

---
+++ RecipeBookApp/script.js
@@ -0,0 +1,15 @@
+const API_KEY = "ec2b6e3ce2894740a0dc2e77b83f1270";
+
+async function getRecipes() {
+  const response = await fetch(
+    `https://api.spoonacular.com/recipes/random?number=10&apikey=${API_KEY}`
+  );
+  const data = await response.json();
+  return data.recipes
+}
+
+async function init() {
+  const recipes = await getRecipes();
+  console.log(recipes);
+}
+init();
