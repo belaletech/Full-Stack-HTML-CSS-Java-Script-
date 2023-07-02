732e0320373f92c17d1a6163f32b6a1b28153e2d
belaletech <belalahmadswnbhr@gmail.com>
2023-07-02 23:46:28 +0530

    update

--- RecipeBookApp/script.js
+++ RecipeBookApp/script.js
@@ -4,8 +4,10 @@ async function getRecipes() {
   const response = await fetch(
     `https://api.spoonacular.com/recipes/random?number=10&apikey=${API_KEY}`
   );
+
   const data = await response.json();
   return data.recipes;
+  console.log(recipes);
 }
 
 async function init() {
